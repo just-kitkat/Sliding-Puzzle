4a7e5ebbc59038de1befd17e5376665167e4e999
kitkat3141 <121209669+kitkat3141@users.noreply.github.com>
2022-12-26 16:59:18 +0800

    modified year in licence

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 """
-Copyright (C) 2023  kitkat3141
+Copyright (C) 2022  kitkat3141
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
