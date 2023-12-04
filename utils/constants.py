cc28ec8b6568b962629043af595a573fc91d3ee1
kitkat3141 <121209669+kitkat3141@users.noreply.github.com>
2023-12-04 14:48:51 +0800

    fix: removed CREDITS var (moved to api)

--- utils/constants.py
+++ utils/constants.py
@@ -1,12 +1,4 @@
 VERSION = "1.0.0"
 
-CREDITS = {
-            "Dev": "JustKitkat",
-            "Art": "Klaide9021",
-            "Language": "Python 3.10.5",
-            "Main Code": "886 Lines",
-            "Music": "@blueqmusic (yt)",
-            }
-
 # Sizes
 FRAME_SIZE_MULT = 1.265
