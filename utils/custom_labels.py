68785f5b7d929ba2e22c7e77ec01ab122435b0cb
kitkat3141 <121209669+just-kitkat@users.noreply.github.com>
2023-12-20 16:34:22 +0800

    new: added NormButtons for factory

--- utils/custom_labels.py
+++ utils/custom_labels.py
@@ -1,12 +1,14 @@
 class NewsLabel:
-    """
-    This is a placeholder to enable text wrapping in the news page
-    """
     pass
 
 
 class WinLabel:
-    """
-    This is a placeholder to enable text sizing in the win screen
-    """
+    pass
+
+
+class NormButton:
+    pass
+
+
+class NormButtonNoSound:
     pass
